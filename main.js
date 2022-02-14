17cb156447aa20b1ca764f99c5cc642dd5fa3a32
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-14 19:57:31 +0600

    second problem solved

--- main.js
+++ main.js
@@ -13,15 +13,26 @@ function isM(arr){
     return result;
 }
 
-console.log(isM(favFood))
+// console.log(isM(favFood))
 
 
-/* ২) একটা অ্যারেতে তোমার বন্ধু বা বান্ধুবির পুরা নাম রাখো এবং যাদের নাম md দিয়ে শুরু হয় সেই নামগুলো প্রথমে console log করবে। তারপর পুরা জিনিসটা একটা ফাংশনে রাখো। এবং তোমার কাজ হবে। যার যার নাম md দিয়ে শুরু হয়। সেই নামগুলো রিটার্ন করবে। 
+/* ২) একটা অ্যারেতে তোমার বন্ধু বা বান্ধুবির পুরা নাম রাখো এবং যাদের নাম md দিয়ে শুরু হয় সেই নামগুলো প্রথমে console log করবে। তারপর পুরা জিনিসটা একটা ফাংশনে রাখো। এবং তোমার কাজ হবে যার যার নাম md দিয়ে শুরু হয়। সেই নামগুলো রিটার্ন করবে। 
 
 এক্সট্রা: চেষ্টা করবে কারো নাম যদি MD বা Md বা md হয়। তাদের নাম যেন তোমার রিটার্ন রেজাল্টে যোগ হয়।  */
 
+let myFriends = ['Md Sangram', 'Md Jamil', 'Yeasin', 'Sakhawat', 'Md Refayet', 'Nuzmul', 'Fahim', 'Arafat']
 
+function incluedMD(arr){
+    let allMD = '';
+    for(const friend of arr){
+        if(friend.toLowerCase().indexOf('md') != -1){
+            allMD += friend + ", ";
+        }
+    }
+    return allMD;
+}
 
+console.log(incluedMD(myFriends))
 
 /* ৩) fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
 
