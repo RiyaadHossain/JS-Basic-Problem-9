a3dd8a7019100e6c5db8f0258e28321816bc1396
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-14 19:45:25 +0600

    first Problem solved

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 
 /* ১) একটা অ্যারেতে তোমার পছন্দের খাবার গুলো রাখো এবং যে সব খাবারের নামের মধ্যে m আছে সেসব খাবারের নামগুলো console log করো। */
+let favFood = ['Ice-cream', 'Malta', 'Mango', 'Biriyani', 'Mutton', 'Chocolate', 'Muri']
 
+function isM(arr){
+    let result = [];
+    for(const name of arr){
+        let demo = name.toLowerCase()
+        if(demo.includes('m')){
+            result.push(name)
+        }
+    }
+    return result;
+}
+
+console.log(isM(favFood))
 
 
 /* ২) একটা অ্যারেতে তোমার বন্ধু বা বান্ধুবির পুরা নাম রাখো এবং যাদের নাম md দিয়ে শুরু হয় সেই নামগুলো প্রথমে console log করবে। তারপর পুরা জিনিসটা একটা ফাংশনে রাখো। এবং তোমার কাজ হবে। যার যার নাম md দিয়ে শুরু হয়। সেই নামগুলো রিটার্ন করবে। 
 
 এক্সট্রা: চেষ্টা করবে কারো নাম যদি MD বা Md বা md হয়। তাদের নাম যেন তোমার রিটার্ন রেজাল্টে যোগ হয়।  */
+
+
 
 
 /* ৩) fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
